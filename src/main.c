2aa1313d8c66482aed78ba14ac1ce20363f9f26c
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-06-10 17:36:12 -0400

    Implemented basic parser

--- src/main.c
+++ src/main.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 
-#include "lexer.h"
+#include "parser.h"
 
 int main() {
-    Lexer lexer = ucc_new_lexer("     27   6");
-
-    Token token = ucc_lexer_next(&lexer);
-    printf("token is integer? %s\ntoken value: ", token.type == TOKEN_TYPE_INTEGER ? "true" : "false");
-    for (int i = 0; i < token.length; i++) {
-        putc(token.token[i], stdout);
+    ParseResult result = ucc_parser_parse("   69   ");
+    if (result.success) {
+        printf("success!\n");
+    } else {
+        printf("error parsing string: %s\n", result.value.error_message);
     }
-    printf("\n");
-
-    token = ucc_lexer_next(&lexer);
-    printf("token is integer? %s\ntoken value: ", token.type == TOKEN_TYPE_INTEGER ? "true" : "false");
-    for (int i = 0; i < token.length; i++) {
-        putc(token.token[i], stdout);
-    }
-    printf("\n");
-
-    token = ucc_lexer_next(&lexer);
-    printf("token is EOF? %s\n", token.type == TOKEN_TYPE_EOF ? "true" : "false");
 }
